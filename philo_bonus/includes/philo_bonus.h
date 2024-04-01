abe14f90e17f575dd56e824adafa04fd5dce73d6
OUSSAMA BENCHKRON SAID <obenchkroune01@gmail.com>
2024-04-01 22:17:10 +0000

    init `philo_bonus`

---
+++ philo_bonus/includes/philo_bonus.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obenchkr <obenchkr@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/01 22:14:25 by obenchkr          #+#    #+#             */
+/*   Updated: 2024/04/01 22:14:50 by obenchkr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_BONUS_H
+# define PHILO_BONUS_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <semaphore.h>
+
+#endif
