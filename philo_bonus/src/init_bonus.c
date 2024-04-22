22ac239ec5191bfab783b15b5e10576f9a57b308
obenchkr <obenchkr@student.1337.ma>
2024-04-22 19:45:15 +0100

    check for sem_open failure

--- philo_bonus/src/init_bonus.c
+++ philo_bonus/src/init_bonus.c
@@ -6,11 +6,25 @@
 /*   By: obenchkr <obenchkr@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 10:31:16 by obenchkr          #+#    #+#             */
-/*   Updated: 2024/04/22 19:25:47 by obenchkr         ###   ########.fr       */
+/*   Updated: 2024/04/22 19:43:39 by obenchkr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
+
+static sem_t	*safe_sem_init(const char *name, int value)
+{
+	sem_t	*sem;
+
+	sem_unlink(name);
+	sem = sem_open(name, O_CREAT | O_EXCL, 0644, value);
+	if (sem == SEM_FAILED)
+	{
+		printf("sem_open failed\n");
+		exit(1);
+	}
+	return (sem);
+}
 
 static t_data	*init_data(int ac, char **av)
 {
@@ -23,16 +37,11 @@ static t_data	*init_data(int ac, char **av)
 	data->time_to_die = ft_atoi(av[2]);
 	data->time_to_eat = ft_atoi(av[3]);
 	data->time_to_sleep = ft_atoi(av[4]);
-	sem_unlink(SEM_FORKS);
-	sem_unlink(SEM_PRINT);
-	sem_unlink(SEM_STOP);
-	sem_unlink(SEM_SYNC);
-	sem_unlink(SEM_MEAL);
-	data->stop_sem = sem_open(SEM_STOP, O_CREAT | O_EXCL, 0644, 1);
-	data->forks = sem_open(SEM_FORKS, O_CREAT | O_EXCL, 0644, data->philo_count);
-	data->print_sem = sem_open(SEM_PRINT, O_CREAT | O_EXCL, 0644, 1);
-	data->sync_sem = sem_open(SEM_SYNC, O_CREAT | O_EXCL, 0644, 1);
-	data->meal_sem = sem_open(SEM_MEAL, O_CREAT | O_EXCL, 0644, 1);
+	data->stop_sem = safe_sem_init(SEM_STOP, 1);
+	data->forks = safe_sem_init(SEM_FORKS, data->philo_count);
+	data->print_sem = safe_sem_init(SEM_PRINT, 1);
+	data->sync_sem = safe_sem_init(SEM_SYNC, 1);
+	data->meal_sem = safe_sem_init(SEM_MEAL, 1);
 	data->max_meals = -1;
 	if (ac == 6)
 		data->max_meals = ft_atoi(av[5]);
