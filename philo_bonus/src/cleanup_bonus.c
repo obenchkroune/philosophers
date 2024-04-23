15744dd30313025a4124c9df90d4c86574e96c68
obenchkr <obenchkr@student.1337.ma>
2024-04-23 03:54:38 +0100

    refactor cleanup fn

--- philo_bonus/src/cleanup_bonus.c
+++ philo_bonus/src/cleanup_bonus.c
@@ -6,28 +6,28 @@
 /*   By: obenchkr <obenchkr@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 15:35:21 by obenchkr          #+#    #+#             */
-/*   Updated: 2024/04/22 19:26:16 by obenchkr         ###   ########.fr       */
+/*   Updated: 2024/04/23 03:54:14 by obenchkr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
+
+static void	cleanup_semaphore(sem_t *sem, char *name)
+{
+	sem_close(sem);
+	sem_unlink(name);
+}
 
 void	cleanup_philo(t_philo *philo)
 {
 	t_data		*data;
 
 	data = philo->data;
-	sem_close(data->forks);
-	sem_close(data->print_sem);
-	sem_close(philo->sem);
-	sem_close(data->stop_sem);
-	sem_close(data->sync_sem);
-	sem_close(data->meal_sem);
-	sem_unlink(SEM_FORKS);
-	sem_unlink(SEM_PRINT);
-	sem_unlink(SEM_STOP);
-	sem_unlink(SEM_SYNC);
-	sem_unlink(SEM_MEAL);
+	cleanup_semaphore(data->forks ,SEM_FORKS);
+	cleanup_semaphore(data->print_sem ,SEM_PRINT);
+	cleanup_semaphore(data->stop_sem, SEM_STOP);
+	cleanup_semaphore(data->sync_sem ,SEM_SYNC);
+	cleanup_semaphore(data->meal_sem ,SEM_MEAL);
 	free(data);
 	free(philo);
 }
